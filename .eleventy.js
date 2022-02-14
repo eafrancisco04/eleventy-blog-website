477d40076f1bb69e253223ff05597d0ab3efe578
Ea Francisco <emberkasai04@gmail.com>
2022-02-14 10:20:48 -0500

    create styling for main page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
+    //Linking to CSS
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addWatchTarget("./src/css");
+    //Add images
+    eleventyConfig.addPassthroughCopy()
+
+    //return Object options
     return {
         dir: {
             input: "src", 
